506f0e2371b4c72f0b38d910338683cb22c43621
Ryu <215513592@qq.com>
2021-02-23 17:39:15 +0800

    提交更新

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-def foo(a=1, b="abc", **kwargs):
+def foo(a, b="abc", **kwargs):
     print('a = ', a)
     print('b = ', b)
     print('kwargs = ', kwargs)
@@ -6,4 +6,4 @@ def foo(a=1, b="abc", **kwargs):
 
 
 if __name__ == '__main__':
-    foo(a=1, c=3)
+    foo()
