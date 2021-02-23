6bdf118390c95996a56a254fbbbd729e76eaf8e9
Ryu <215513592@qq.com>
2021-02-23 16:22:11 +0800

    添加pycharm项目相关的配置文件

--- 03_imdb.py
+++ 03_imdb.py
@@ -15,6 +15,8 @@ from keras import metrics
 import matplotlib.pyplot as plt
 
 # 载入数据集
+# (train_data, train_labels), (test_data, test_labels) = imdb.load_data(
+#    num_words=10000)
 (train_data, train_labels), (test_data, test_labels) = imdb.load_data(
     num_words=10000)
 
