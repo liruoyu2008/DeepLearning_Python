73906724db960c62bf62ac011f8bbd55b565d2ef
Ryu <215513592@qq.com>
2021-02-28 23:20:58 +0800

    提交测试是否再GPU上运行的测试代码

--- test.py
+++ test.py
@@ -1,25 +1,7 @@
-def foo(a=1, b="abc", **kwargs):
-    print('a = ', a)
-    print('b = ', b)
-    print('kwargs = ', kwargs)
-    print('---------------------------------------')
+import os
 
-
-def create_labels(filepath:str):
-    """根据给定的文件路径生成标签文件
-
-    Args:
-        filepath (str): 文件完整路径，包含文件名
-    """    
-    f = open(filepath, 'w')
-    list1 = ['1\n' for i in range(0, 100)]
-    list2 = ['2\n' for i in range(0, 100)]
-    list = list1+list2
-    f.writelines(list)
-    f.flush()
-    f.close()
-    print('success!')
-
-
-if __name__ == '__main__':
-    create_labels(r'C:\Users\Ryu\Desktop\train\labels.txt')
+# 测试程序是否再GPU上运行
+import tensorflow as tf
+print(tf.config.list_physical_devices('GPU'))
+print('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
+print(tf.test.is_built_with_cuda())
