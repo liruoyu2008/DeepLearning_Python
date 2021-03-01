d939f301f9b45bbdd93e15abc1b8de4aae7100eb
Ryu <215513592@qq.com>
2021-03-01 11:16:41 +0800

    更新最新训练的模型

--- 05_dogs_cats.py
+++ 05_dogs_cats.py
@@ -136,7 +136,7 @@ validation_generator = test_datagen.flow_from_directory(
 history = model.fit_generator(
     train_generator,
     steps_per_epoch=100,
-    epochs=30,
+    epochs=100,
     validation_data=validation_generator,
     validation_steps=50)
 
