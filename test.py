79b90b25a6399fd78176d4e50dc711ee56d78e76
Ryu <215513592@qq.com>
2021-03-02 22:34:46 +0800

    Ceshi Git速度

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import os
 
-# tf2.1下测试程序是否再GPU上运行
+# tf2.1下测试程序是否再GPU上运行 
 import tensorflow as tf
 print(tf.config.list_physical_devices('GPU'))
 print('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
