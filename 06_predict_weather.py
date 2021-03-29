e59fc7e390ca85c913be11dd1994cdb873513551
Ryu <215513592@qq.com>
2021-03-29 18:46:59 +0800

    提交基于GRU、LSTM并dropout的天气预测代码

--- 06_predict_weather.py
+++ 06_predict_weather.py
@@ -2,6 +2,9 @@ import os
 from os.path import split
 import numpy as np
 from matplotlib import pyplot as plt
+from keras.models import Sequential
+from keras import layers
+from keras.optimizers import RMSprop
 
 # 读取天气数据
 data_dir = '/users/Ryu/Downloads/jena_climate'
@@ -28,15 +31,16 @@ for i, line in enumerate(lines):
     float_data[i, :] = values
 
 # 选取温度样本列并作图
-temp = float_data[:, 1]  # 温度（单位：摄氏度）
-plt.plot(range(len(temp)), temp)
-plt.show()
+# temp = float_data[:, 1]  # 温度（单位：摄氏度）
+# plt.plot(range(len(temp)), temp)
+# plt.show()
 
 # 将前20w个数据标准化
 mean = float_data[:200000].mean(axis=0)
 float_data -= mean
 std = float_data[:200000].std(axis=0)
 float_data /= std
+
 
 def generator(data, lookback, delay, min_index, max_index,
               shuffle=False, batch_size=128, step=6):
@@ -76,3 +80,129 @@ def generator(data, lookback, delay, min_index, max_index,
             samples[j] = data[indices]
             targets[j] = data[rows[j] + delay][1]
         yield samples, targets
+
+
+# 训练数据、评估数据、测试数据的迭代器
+lookback = 1440
+step = 6
+delay = 144
+batch_size = 128
+train_gen = generator(float_data,
+                      lookback=lookback,
+                      delay=delay,
+                      min_index=0,
+                      max_index=200000,
+                      shuffle=True,
+                      step=step,
+                      batch_size=batch_size)
+val_gen = generator(float_data,
+                    lookback=lookback,
+                    delay=delay,
+                    min_index=200001,
+                    max_index=300000,
+                    step=step,
+                    batch_size=batch_size)
+test_gen = generator(float_data,
+                     lookback=lookback,
+                     delay=delay,
+                     min_index=300001,
+                     max_index=None,
+                     step=step,
+                     batch_size=batch_size)
+val_steps = (300000 - 200001 - lookback) // batch_size
+test_steps = (len(float_data) - 300001 - lookback) // batch_size
+
+
+def evaluate_naive_method():
+    """符合常识的基准MAE评估方法
+    """
+    batch_maes = []
+    for step in range(val_steps):
+        samples, targets = next(val_gen)
+        preds = samples[:, -1, 1]
+        mae = np.mean(np.abs(preds - targets))
+        batch_maes.append(mae)
+    print(np.mean(batch_maes))
+
+
+# 开始使用基准评估
+evaluate_naive_method()
+
+# 先使用简单的密集连接模型进行回归（参照前面的boston_housing示例）
+# model = Sequential()
+# model.add(layers.Flatten(input_shape=(lookback // step, float_data.shape[-1])))
+# model.add(layers.Dense(32, activation='relu'))
+# model.add(layers.Dense(1))
+# model.compile(optimizer=RMSprop(), loss='mae')
+# history = model.fit_generator(train_gen,
+#                               steps_per_epoch=500,
+#                               epochs=20,
+#                               validation_data=val_gen,
+#                               validation_steps=val_steps)
+
+# 绘制结果
+# loss = history.history['loss']
+# val_loss = history.history['val_loss']
+# epochs = range(1, len(loss) + 1)
+# plt.figure()
+# plt.plot(epochs, loss, 'bo', label='Training loss')
+# plt.plot(epochs, val_loss, 'b', label='Validation loss')
+# plt.title('Training and validation loss')
+# plt.legend()
+# plt.show()
+
+# 训练并评估一个基于GRU（是LSTM降低了计算代价和表示能力的折中方案）并使用dropout的模型
+# model = Sequential()
+# model.add(layers.GRU(32,
+#                      dropout=0.2,
+#                      recurrent_dropout=0.2,
+#                      input_shape=(None, float_data.shape[-1])))
+# model.add(layers.Dense(1))
+# model.compile(optimizer=RMSprop(), loss='mae')
+# history = model.fit_generator(train_gen,
+#                               steps_per_epoch=500,
+#                               epochs=40,
+#                               validation_data=val_gen,
+#                               validation_steps=val_steps)
+
+# 绘制结果
+# loss = history.history['loss']
+# val_loss = history.history['val_loss']
+# epochs = range(1, len(loss) + 1)
+# plt.figure()
+# plt.plot(epochs, loss, 'bo', label='Training loss')
+# plt.plot(epochs, val_loss, 'b', label='Validation loss')
+# plt.title('Training and validation loss')
+# plt.legend()
+# plt.show()
+
+# 训练并评估一个基于LSTM并使用dropout的模型，并增大网络容量
+model = Sequential()
+model.add(layers.GRU(32,
+                     dropout=0.1,
+                     recurrent_dropout=0.5,
+                     return_sequences=True,
+                     input_shape=(None, float_data.shape[-1])))
+model.add(layers.GRU(64, activation='relu',
+                     dropout=0.1,
+                     recurrent_dropout=0.5))
+model.add(layers.Dense(1))
+model.compile(optimizer=RMSprop(), loss='mae')
+history = model.fit_generator(train_gen,
+                              steps_per_epoch=500,
+                              epochs=40,
+                              validation_data=val_gen,
+                              validation_steps=val_steps)
+
+# 绘制结果
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(1, len(loss) + 1)
+plt.figure()
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+plt.show()
+
+#  
