d2c067c6b1e6ee3f326022791277c435edcf6dfc
Ryu <215513592@qq.com>
2021-03-30 17:59:21 +0800

    提交双向的LSTM层代码

--- 06_imdb_LSTM.py
+++ 06_imdb_LSTM.py
@@ -4,7 +4,6 @@ from keras import layers
 from keras.models import Sequential
 import matplotlib.pyplot as plt
 
-# 使用逆序序列评估一个LSTM模型
 max_features = 10000
 maxlen = 500
 (x_train, y_train), (x_test, y_test) = imdb.load_data(
@@ -13,13 +12,41 @@ x_train = [x[::-1] for x in x_train]
 x_test = [x[::-1] for x in x_test]
 x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
 x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
+
+# 使用逆序序列评估一个LSTM模型
+# model = Sequential()
+# model.add(layers.Embedding(max_features, 128))
+# model.add(layers.LSTM(32))
+# model.add(layers.Dense(1, activation='sigmoid'))
+# model.compile(optimizer='rmsprop',
+#               loss='binary_crossentropy',
+#               metrics=['acc'])
+# history = model.fit(x_train, y_train,
+#                     epochs=10,
+#                     batch_size=128,
+#                     validation_split=0.2)
+# acc = history.history['acc']
+# val_acc = history.history['val_acc']
+# loss = history.history['loss']
+# val_loss = history.history['val_loss']
+# epochs = range(1, len(acc) + 1)
+# plt.plot(epochs, acc, 'bo', label='Training acc')
+# plt.plot(epochs, val_acc, 'b', label='Validation acc')
+# plt.title('Training and validation accuracy')
+# plt.legend()
+# plt.figure()
+# plt.plot(epochs, loss, 'bo', label='Training loss')
+# plt.plot(epochs, val_loss, 'b', label='Validation loss')
+# plt.title('Training and validation loss')
+# plt.legend()
+# plt.show()
+
+# 训练并评估一个双向的LSTM
 model = Sequential()
-model.add(layers.Embedding(max_features, 128))
-model.add(layers.LSTM(32))
+model.add(layers.Embedding(max_features, 32))
+model.add(layers.Bidirectional(layers.LSTM(32)))
 model.add(layers.Dense(1, activation='sigmoid'))
-model.compile(optimizer='rmsprop',
-              loss='binary_crossentropy',
-              metrics=['acc'])
+model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
 history = model.fit(x_train, y_train,
                     epochs=10,
                     batch_size=128,
