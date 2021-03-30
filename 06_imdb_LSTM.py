14dade2a19409b7913ed5e6dd4d893edb90196c3
Ryu <215513592@qq.com>
2021-03-30 17:00:39 +0800

    提交基于LSTM的imdb影评代码

---
+++ 06_imdb_LSTM.py
@@ -0,0 +1,41 @@
+from keras.datasets import imdb
+from keras.preprocessing import sequence
+from keras import layers
+from keras.models import Sequential
+import matplotlib.pyplot as plt
+
+# 使用逆序序列评估一个LSTM模型
+max_features = 10000
+maxlen = 500
+(x_train, y_train), (x_test, y_test) = imdb.load_data(
+    num_words=max_features)
+x_train = [x[::-1] for x in x_train]
+x_test = [x[::-1] for x in x_test]
+x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
+x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
+model = Sequential()
+model.add(layers.Embedding(max_features, 128))
+model.add(layers.LSTM(32))
+model.add(layers.Dense(1, activation='sigmoid'))
+model.compile(optimizer='rmsprop',
+              loss='binary_crossentropy',
+              metrics=['acc'])
+history = model.fit(x_train, y_train,
+                    epochs=10,
+                    batch_size=128,
+                    validation_split=0.2)
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(1, len(acc) + 1)
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+plt.figure()
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+plt.show()
