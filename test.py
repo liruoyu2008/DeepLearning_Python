9f1bc9ad6c1fa7ba91972292826783d10196db8d
Ryu <215513592@qq.com>
2021-12-10 17:22:57 +0800

    提交opencvc测试代码

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
-a = 1;
+import numpy as np
+import cv2 as cv
 
-def b():
-    print(2)
+img = cv.imread('1.jpg')
+print(img)
+cv.namedWindow('123')
+cv.imshow('123',img)
+cv.waitKey(0)
+cv.destroyAllWindows()
