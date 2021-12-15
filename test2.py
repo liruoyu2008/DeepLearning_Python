5c5004bf42c19c1efde485eafdaad899546ad06c
Ryu <215513592@qq.com>
2021-12-15 16:39:45 +0800

    提交修改

--- test2.py
+++ test2.py
@@ -1,4 +1,10 @@
-with open('test.py','a+') as f:
-    for i in range(10):
-        print('123',file=f)
-        
+import sys
+
+
+a = 1
+b = 0
+try:
+    c = a/b
+except:
+    x = sys.exc_info()
+    d = 0
